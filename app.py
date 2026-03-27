1ec003c6959eb7004db203c2b5bd8bbd7dd26d7e
HUYNH KIM TUYEN <huynhkimtuyenphuyen@gmail.com>
2026-03-27 12:44:34 +0700

    cập nhật thuật toán

---
+++ app.py
@@ -0,0 +1,47 @@
+﻿import warnings
+import joblib
+
+warnings.filterwarnings("ignore", message="X does not have valid feature names")
+
+danh_sach = [
+    "Dùng địa chỉ IP thay cho tên miền (having_IP_Address).",
+    "Đường dẫn URL quá dài, có dấu hiệu ẩn giấu (URL_Length).",
+    "Sử dụng dịch vụ rút gọn link để che mắt (Shortining_Service).",
+    "Có chứa ký tự @ đáng ngờ trong đường dẫn (having_At_Symbol).",
+    "Chuyển hướng người dùng bằng dấu // (double_slash_redirecting).",
+    "Tên miền có chứa dấu gạch ngang - (Prefix_Suffix).",
+    "Sử dụng quá nhiều tên miền phụ (having_Sub_Domain).",
+    "Chứng chỉ bảo mật SSL không hợp lệ hoặc không có (SSLfinal_State).",
+    "Thời gian đăng ký tên miền quá ngắn (Domain_registeration_length).",
+    "URL yêu cầu tải tài nguyên từ trang web khác (Request_URL).",
+    "Đường dẫn neo (Anchor) trỏ sang trang lạ (URL_of_Anchor).",
+    "Các thẻ Meta/Script chứa liên kết ẩn (Links_in_tags).",
+    "Biểu mẫu (Form) gửi dữ liệu đi nơi khác (SFH).",
+    "Lưu lượng truy cập web thấp hoặc không xác định (web_traffic).",
+    "Trang web không được Google lập chỉ mục (Google_Index)."
+]
+
+# 1. Gọi AI
+ai_da_tot_nghiep = joblib.load('models/random_forest_15.pkl')
+
+# 2. Khách hàng nhập link
+#link test
+link_khach_nhap = "http://web-nhan-qua-shopee-gia-mao.com"
+
+# 3. Tính toán ra mảng 15 đặc trưng (Gọi từ feature_extractor của đệ)
+mang_15_dac_trung = [[-1, 1, 1, 1, -1, -1, -1, -1, -1, 1, -1, 1, -1, -1, 1]] 
+
+# 4. AI kiểm tra
+ket_qua = ai_da_tot_nghiep.predict(mang_15_dac_trung)
+
+# 5. XỬ LÝ ĐẦU RA 
+if ket_qua[0] == -1:
+    print(f"❌ CẢNH BÁO: {link_khach_nhap} là WEB LỪA ĐẢO!")
+    print("🔎 LÝ DO :")
+    
+    cac_con_so = mang_15_dac_trung[0]
+    for vi_tri in range(len(cac_con_so)):
+        if cac_con_so[vi_tri] == -1:
+            print(f"  - {danh_sach[vi_tri]}")
+else:
+    print(f"✅ AN TOÀN: {link_khach_nhap} là web sạch!")
---
+++ train_model.py
@@ -0,0 +1,47 @@
+﻿import pandas as pd
+from scipy.io import arff
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+import joblib
+import os
+
+print("--- BẮT ĐẦU KIỂM TRA ---")
+file_path = "data/dataset_15.arff"
+print(f"1. Đang đọc dữ liệu từ {file_path} ...")
+data, meta = arff.loadarff(file_path)
+df = pd.DataFrame(data)
+
+df = df.apply(lambda col: col.astype(int))
+
+X = df.drop("Result", axis=1) # X lấy 15 cột đề bài
+y = df["Result"]              # y lấy cột kết quả cuối cùng
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+print("2. Đang truyền cho Random Forest...")
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+
+model.fit(X_train, y_train)
+
+print("3. Đang cho AI kiểm tra trên tập dữ liệu...")
+y_pred = model.predict(X_test)
+
+
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred, pos_label=-1) 
+recall = recall_score(y_test, y_pred, pos_label=-1)
+
+print("\n========== BẢNG VÀNG THÀNH TÍCH ==========")
+print(f"Độ chính xác (Accuracy)  : {accuracy * 100:.2f}%")
+print(f"Độ tin cậy   (Precision) : {precision * 100:.2f}%")
+print(f"Độ nhạy      (Recall)    : {recall * 100:.2f}%")
+print("==========================================\n")
+
+# # 7. Đóng gói 
+# print("4. Đang đóng gói trí tuệ nhân tạo thành file...")
+# if not os.path.exists('models'):
+#     os.makedirs('models') # Tạo thư mục models nếu chưa tạo
+    
+# joblib.dump(model, 'models/random_forest_15.pkl') # Lưu tên file có số 15 để phân biệt
+# print("=> ĐẠI CÔNG CÁO THÀNH! Đã lưu mô hình tại: models/random_forest_15.pkl")
